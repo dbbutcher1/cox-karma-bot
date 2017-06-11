16fa3577d21204d0c8eab44b85a557a13c5c01e3
David Butcher <dbbutcher1@gmail.com>
2017-06-11 17:08:18 -0400

    Begin adding commands

--- bot/karma_bot.rb
+++ bot/karma_bot.rb
@@ -1,7 +1,15 @@
 class KarmaBot < SlackRubyBot::Bot
 
   # Command for adding karma
+  match /\+{2,}/ do |client, data, match|
+
+  end
 
   # Command for removing karma
+  match /\-{2,}/ do |client, data, match|
 
+  end
+
+  command /leaderboard/ do |client, data, match|
+  end
 end
