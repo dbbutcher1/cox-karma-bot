78a94718ff980b01bfc1a4e159b795fcc349c6c9
David Butcher <dbutcher@coxautoinc.com>
2017-07-06 14:15:05 -0400

    Removing puma config from prod deploy file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -68,14 +68,3 @@ ssh_options: {
   forward_agent: true,
   auth_methods: %w(publickey)
 }
-
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :puma_role, :app
-set :puma_conf, "#{shared_path}/puma.rb"
