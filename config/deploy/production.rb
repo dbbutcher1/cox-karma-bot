e42f22c7efaecfafca18a2a60ad1ba0cbb66519c
David Butcher <dbutcher@coxautoinc.com>
2017-07-06 13:31:07 -0400

    More updates

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -21,7 +21,7 @@
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-role :web, %{localhost}
+role :web, %{ENV['DEPLOY_SERVER_IP']}
 
 # Configuration
 # =============
