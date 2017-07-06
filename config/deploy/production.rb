2ebaedff32188ebd1ff4e61058345f2a2ba692e8
David Butcher <dbutcher@coxautoinc.com>
2017-07-06 14:02:27 -0400

    Puma conf

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -78,3 +78,4 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :puma_role, :app
+set :puma_conf, "#{shared_path}/puma.rb"
